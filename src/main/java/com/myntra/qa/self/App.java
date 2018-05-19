0b233135747607466d0ae29202152f855add3cdf
kanchan Bangar <36080415+kanchanSBangar@users.noreply.github.com>
2018-05-19 17:50:47 +0530

    first commit

---
+++ src/main/java/com/myntra/qa/self/App.java
@@ -0,0 +1,13 @@
+package com.myntra.qa.self;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
